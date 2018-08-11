2e4694c08733023af3871e5ea171b9395165d189
Ehrest <39272186+Ehrest@users.noreply.github.com>
2018-08-11 16:54:56 +0200

    [Code] Level Manager base, Item base

---
+++ Running out of space/Assets/Scripts/Item/ScriptableObjects/ItemDescription.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemDescription", menuName = "ItemData/Description", order = 1)]
+public class ItemDescription : ScriptableObject
+{
+    public GameObject Prefab;
+
+    public ItemType ItemType;
+    public ItemContainerType ContainerType;
+    public int ContainSlot;
+    public int ContentSize;
+    public Vector3 Size;
+}
