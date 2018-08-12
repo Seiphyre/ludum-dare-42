f19c84d855b594b7706163f341c93da42a3c9520
rlesert <lesert@mycyberroyaume.com>
2018-08-12 13:10:05 +0200

    Billboard du canvas de contenu d'un meuble

---
+++ Running out of space/Assets/Scripts/Utility/LookAtScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtScript : MonoBehaviour
+{
+
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (target == null)
+            Debug.LogError("No target for " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target != null)
+            transform.forward = target.forward;
+    }
+}
