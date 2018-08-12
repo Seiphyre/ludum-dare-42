81b2b4719e7edb7d2a587b2772fef5668020141d
dannee_v <dannee_v@epitech.eu>
2018-08-12 03:24:50 +0200

    can rotate an object in player and and detect if the object collide with an other object

---
+++ Running out of space/Assets/Scripts/EntityTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityTest : MonoBehaviour {
+
+	// Use this for initialization
+	private void Awake()
+	{
+		GetComponent<ItemEntity>().InitializeTest();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
